66f4cb81d649b0991b62f09f7862046ffffc083f
Alexander Sologub <alexsologoub@gmail.com>
2018-03-15 13:36:07 +0300

    Automate dpkg configure

--- colab-setup.sh
+++ colab-setup.sh
@@ -1,4 +1,7 @@
 #/bin/bash!
-apt-get install python3.6-tk
+export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
+
+apt-get install -y python3.6-tk nano
 pip3 install http://download.pytorch.org/whl/cu80/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
 pip3 install -r requirements.txt
