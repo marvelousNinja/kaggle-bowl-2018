072c529a60a1bae68e2a2794d472550d7d4f6bea
Alexander Sologub <alexsologoub@gmail.com>
2018-03-16 10:44:37 +0300

    Add a couple of tools to setup

--- colab-setup.sh
+++ colab-setup.sh
@@ -2,6 +2,6 @@
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
-apt-get install -y python3.6-tk nano
+apt-get install -y python3.6-tk nano htop screen
 pip3 install http://download.pytorch.org/whl/cu80/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
 pip3 install -r requirements.txt
