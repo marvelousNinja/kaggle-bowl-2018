934f23c36ffd7a47013a9176da8b91d43ab4ae38
Alexander Sologub <alexsologoub@gmail.com>
2018-03-19 17:06:40 +0300

    Extract toy shapes generator from utils

---
+++ bowl/toy_shapes.py
@@ -0,0 +1,88 @@
+import numpy as np
+from PIL import Image, ImageDraw
+
+from bowl.utils import normalize
+
+def random_bbox_in(shape):
+    x0 = np.random.randint(shape[0])
+    y0 = np.random.randint(shape[1])
+    width = np.random.randint(16, int(shape[0] / 5))
+    height = np.random.randint(16, int(shape[1] / 5))
+    x1, y1 = x0 + width, y0 + height
+    return [x0, y0, x1, y1]
+
+def intersects_with_any(bbox, other_bboxes):
+    return any(intersects(bbox, other_bbox) for other_bbox in other_bboxes)
+
+def intersects(bbox, other_bbox):
+    return range_overlap(bbox[0], bbox[2], other_bbox[0], other_bbox[2]) and range_overlap(bbox[1], bbox[3], other_bbox[1], other_bbox[3])
+
+def range_overlap(a_min, a_max, b_min, b_max):
+    return (a_min <= b_max) and (b_min <= a_max)
+
+def generate_segmentation_image(shape):
+    image = Image.new('RGB', shape, 0)
+    bboxes = []
+    masks = []
+    draw = ImageDraw.Draw(image)
+
+    max_tries = 35
+    tries = 0
+
+    while tries <= max_tries:
+        tries += 1
+        mask = Image.new('L', shape, 0)
+        mask_draw = ImageDraw.Draw(mask)
+        bbox = random_bbox_in(shape)
+
+        if intersects_with_any(bbox, bboxes):
+            tries += 1
+            continue
+
+        random_color = tuple(np.random.randint(10, 255, 3))
+        if np.random.rand() > 0.5:
+            draw.ellipse(bbox, fill=random_color)
+            mask_draw.ellipse(bbox, fill=255)
+        else:
+            draw.rectangle(bbox, fill=random_color)
+            mask_draw.rectangle(bbox, fill=255)
+
+        x0, x1 = bbox[::2]
+        y0, y1 = bbox[1::2]
+        x0 -= 2
+        x1 += 1
+        y0 -= 2
+        y1 += 1
+        x0, x1 = np.clip([x0, x1], 0, shape[1])
+        y0, y1 = np.clip([y0, y1], 0, shape[0])
+        bboxes.append([x0, y0, x1, y1])
+        masks.append(mask)
+
+    return np.array(image), np.array(bboxes), np.array(list(map(np.array, masks))) / 255
+
+def generate_segmentation_batch(size):
+    images = []
+    gt_boxes = []
+    masks = []
+
+    for _ in range(size):
+        image, bboxes, image_masks = generate_segmentation_image((224, 224))
+        image = np.swapaxes(image, 0, 2)
+        images.append(image)
+        gt_boxes.append(bboxes)
+        masks.append(image_masks)
+
+    images = np.array(images)
+    images = normalize(images)
+    gt_boxes = np.array(gt_boxes)
+    masks = np.array(masks)
+
+    return images, gt_boxes, masks
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    plt.imshow(generate_segmentation_image((512, 512))[0])
+    plt.show()
+    images = np.array([generate_segmentation_image((512, 512))[0] for i in range(100)])
+    print(f'Mean: {(images / 255).mean(axis=(1, 2)).mean(axis=0)}')
+    print(f'Std: {(images / 255).std(axis=(1, 2)).mean(axis=0)}')
--- bowl/utils.py
+++ bowl/utils.py
@@ -1,72 +1,114 @@
 import numpy as np
-from PIL import Image, ImageDraw
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import torch
+from torch.autograd import Variable
 
-def random_bbox_in(shape):
-    x0 = np.random.randint(shape[0])
-    y0 = np.random.randint(shape[1])
-    width = np.random.randint(16, int(shape[0] / 5))
-    height = np.random.randint(16, int(shape[1] / 5))
-    x1, y1 = x0 + width, y0 + height
-    return [x0, y0, x1, y1]
+def normalize(image_batch):
+    image_batch = image_batch.astype(np.float32)
+    image_batch /= 255
+    mean = [0.08734627, 0.08734627, 0.08734627]
+    std = [0.28179365, 0.28179365, 0.28179365]
+    image_batch[:, 0, :, :] -= mean[0]
+    image_batch[:, 1, :, :] -= mean[1]
+    image_batch[:, 2, :, :] -= mean[2]
+    image_batch[:, 0, :, :] /= std[0]
+    image_batch[:, 1, :, :] /= std[1]
+    image_batch[:, 2, :, :] /= std[2]
+    return image_batch
 
-def intersects_with_any(bbox, other_bboxes):
-    return any(intersects(bbox, other_bbox) for other_bbox in other_bboxes)
+def construct_deltas(gt_boxes, anchors):
+    gt_boxes = gt_boxes.astype(np.float32)
+    w_gt = gt_boxes[:, 2] - gt_boxes[:, 0]
+    h_gt = gt_boxes[:, 3] - gt_boxes[:, 1]
+    w_a = anchors[:, 2] - anchors[:, 0]
+    h_a = anchors[:, 3] - anchors[:, 1]
+    t_x = (gt_boxes[:, 0] - anchors[:, 0]) / w_a
+    t_y = (gt_boxes[:, 1] - anchors[:, 1]) / h_a
+    t_w = np.log(w_gt / w_a)
+    t_h = np.log(h_gt / h_a)
+    return np.column_stack((t_x, t_y, t_w, t_h))
 
-def intersects(bbox, other_bbox):
-    return range_overlap(bbox[0], bbox[2], other_bbox[0], other_bbox[2]) and range_overlap(bbox[1], bbox[3], other_bbox[1], other_bbox[3])
+def construct_boxes(deltas, anchors):
+    w_a = anchors[:, 2] - anchors[:, 0]
+    h_a = anchors[:, 3] - anchors[:, 1]
+    x_a = anchors[:, 0]
+    y_a = anchors[:, 1]
+    return np.column_stack((
+        deltas[:, 0] * w_a + x_a,
+        deltas[:, 1] * h_a + y_a,
+        deltas[:, 0] * w_a + x_a + np.exp(deltas[:, 2]) * w_a,
+        deltas[:, 1] * h_a + y_a + np.exp(deltas[:, 3]) * h_a
+    ))
 
-def range_overlap(a_min, a_max, b_min, b_max):
-    return (a_min <= b_max) and (b_min <= a_max)
+def generate_anchor_grid(base, scales, ratios, grid_shape):
+    anchors = []
+    anchors = np.zeros((grid_shape[0], grid_shape[1], len(scales) * len(ratios), 4), dtype=np.float32)
+    for y_diff in range(grid_shape[0]):
+        for x_diff in range(grid_shape[0]):
+            for scale in scales:
+                anchor_index = 0
+                for ratio in ratios:
+                    width = int(base / ratio * scale)
+                    height = int(base * ratio * scale)
+                    x_ctr = int(base / 2) + x_diff * base
+                    y_ctr = int(base / 2) + y_diff * base
+                    x1, y1 = x_ctr - width / 2, y_ctr - height / 2
+                    x2, y2 = x_ctr + width / 2, y_ctr + height / 2
 
-def generate_segmentation_image(shape):
-    image = Image.new('RGB', shape, 0)
-    bboxes = []
-    masks = []
-    draw = ImageDraw.Draw(image)
+                    anchors[y_diff, x_diff, anchor_index] = [x1, y1, x2, y2]
+                    anchor_index += 1
 
-    max_tries = 35
-    tries = 0
+    return np.clip(anchors - 1, a_min=0, a_max=None)
 
-    while tries <= max_tries:
-        tries += 1
-        mask = Image.new('L', shape, 0)
-        mask_draw = ImageDraw.Draw(mask)
-        bbox = random_bbox_in(shape)
+def iou(bboxes_a, bboxes_b):
+    tl = np.maximum(bboxes_a[:, None, :2], bboxes_b[:, :2])
+    br = np.minimum(bboxes_a[:, None, 2:], bboxes_b[:, 2:])
+    area_i = np.prod(br - tl, axis=2) * (tl < br).all(axis=2)
+    area_a = np.prod(bboxes_a[:, 2:] - bboxes_a[:, :2], axis=1)
+    area_b = np.prod(bboxes_b[:, 2:] - bboxes_b[:, :2], axis=1)
+    return area_i / (area_a[:, None] + area_b - area_i)
 
-        if intersects_with_any(bbox, bboxes):
-            tries += 1
-            continue
+def display_image_and_boxes(image, boxes, masks=None):
+    boxes = np.clip(boxes, 0, 223).astype(np.int32)
 
-        random_color = tuple(np.random.randint(10, 255, 3))
-        if np.random.rand() > 0.5:
-            draw.ellipse(bbox, fill=random_color)
-            mask_draw.ellipse(bbox, fill=255)
-        else:
-            draw.rectangle(bbox, fill=random_color)
-            mask_draw.rectangle(bbox, fill=255)
+    plt.subplot(121)
+    plt.cla()
+    _, ax = plt.gcf(), plt.gca()
+    ax.imshow(np.swapaxes(image, 0, 2))
 
-        x0, x1 = bbox[::2]
-        y0, y1 = bbox[1::2]
-        x0 -= 2
-        x1 += 1
-        y0 -= 2
-        y1 += 1
+    for (x1, y1, x2, y2) in boxes:
+        rect = patches.Rectangle((x1, y1), x2 - x1, y2 - y1,linewidth=1,edgecolor='r',facecolor='none')
+        ax.add_patch(rect)
 
-        # x0, x1 = np.clip(bbox[::2], 0, shape[0])
-        # y0, y1 = np.clip(bbox[1::2], 0, shape[1])
-        # y0, y1
+    if masks is not None:
+        plt.subplot(122)
+        full = np.zeros(shape=image.shape[1:3])
+        for (x1, y1, x2, y2), mask in zip(boxes, masks):
+            height = y2 - y1
+            width = x2 - x1
 
+            if height > 0 and width > 0:
+                torch_mask = Variable(torch.from_numpy(mask))
+                predicted_mask = torch.nn.functional.upsample(torch_mask.unsqueeze(dim=0), size=(int(height), int(width)), mode='bilinear')[0][0]
+                predicted_mask = predicted_mask.round().data.cpu().numpy()
+                full[y1:y2, x1:x2] = predicted_mask
 
+        plt.imshow(full)
 
-        bboxes.append([x0, y0, x1, y1])
-        masks.append(mask)
+    plt.draw()
+    plt.pause(1e-17)
 
-    return np.array(image), np.array(bboxes), np.array(list(map(np.array, masks))) / 255
+def as_cuda(variable):
+    if torch.cuda.is_available():
+        return variable.cuda()
+    return variable
 
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    # plt.imshow(generate_segmentation_image((512, 512))[0])
-    # plt.show()
-    images = np.array([generate_segmentation_image((512, 512))[0] for i in range(100)])
-    print(f'Mean: {(images / 255).mean(axis=(1, 2)).mean(axis=0)}')
-    print(f'Std: {(images / 255).std(axis=(1, 2)).mean(axis=0)}')
+def from_numpy(obj, dtype=np.float32):
+    variable = Variable(torch.from_numpy(obj.astype(dtype)))
+    return as_cuda(variable)
+
+def to_numpy(variable):
+    return variable.data.cpu().numpy()
