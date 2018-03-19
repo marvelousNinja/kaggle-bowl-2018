141995d7ce36747306382a14d0336cbd4cdc5dd8
Alexander Sologub <alexsologoub@gmail.com>
2018-03-19 18:20:41 +0300

    Fix Colaboratory setup scripts

--- make.sh
+++ make.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -x
-set -e
-
-CUDA_PATH=/usr/lib/x86_64-linux-gnu
 
 echo "Compiling crop_and_resize kernels by nvcc..."
 cd roi_align/src/cuda
-$CUDA_PATH/bin/nvcc -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_37
+nvcc -ccbin clang-3.8 -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_37
 
 cd ../../
-python build.py
+python3 build.py
