64ada491f3928d6545eb23382f8c0721a5300607
Alexander Sologub <alexsologoub@gmail.com>
2018-03-28 21:39:43 +0300

    Fix NMS compilationy

--- make.sh
+++ make.sh
@@ -10,7 +10,7 @@ cd ../
 
 cd nms/src/cuda
 echo "Compiling nms kernels by nvcc..."
-nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_37
+nvcc -ccbin clang-3.8 -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_37
 cd ../../
-python build.py
+python3 build.py
 cd ../
