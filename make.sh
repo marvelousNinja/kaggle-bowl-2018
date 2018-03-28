a9edc57c33257db1fc6ccd65110ec30bb5a089ed
Alexander Sologub <alexsologoub@gmail.com>
2018-03-28 14:47:50 +0300

    Add a GPU implementation for NMS

--- make.sh
+++ make.sh
@@ -4,6 +4,13 @@ set -x
 echo "Compiling crop_and_resize kernels by nvcc..."
 cd roi_align/src/cuda
 nvcc -ccbin clang-3.8 -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_37
-
 cd ../../
 python3 build.py
+cd ../
+
+cd nms/src/cuda
+echo "Compiling nms kernels by nvcc..."
+nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_37
+cd ../../
+python build.py
+cd ../
