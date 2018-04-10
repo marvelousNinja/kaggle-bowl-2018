757f9b556ece77ce3ce33b7180d4ca9f027c4d95
Alexander Sologub <alexsologoub@gmail.com>
2018-04-10 23:57:32 +0300

    Disabling parallel processing for now

--- bowl/generators.py
+++ bowl/generators.py
@@ -13,12 +13,13 @@ def toy_shapes_generator(image_shape=(224, 224)):
         yield generate_segmentation_batch(1, shape=image_shape)
 
 def bowl_train_generator(image_shape=(224, 224)):
-    pool = ThreadPool()
+    # TODO AS: Disabling multi-threading for now 'cause of OOM on Colab
+    # pool = ThreadPool()
     train_ids = get_train_image_ids()
 
     while True:
         np.random.shuffle(train_ids)
-        yield from pool.imap(partial(pipeline, image_shape), train_ids)
+        yield from map(partial(pipeline, image_shape), train_ids)
 
 def bowl_validation_generator(image_shape=(224, 224)):
     validation_ids = get_validation_image_ids()
