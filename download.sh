751cf8169d6bc463a7b07cbf9656f9ff351513e3
Alexander Sologub <alexsologoub@gmail.com>
2018-04-11 20:33:14 +0300

    Add fixed dataset

--- download.sh
+++ download.sh
@@ -11,3 +11,5 @@ kaggle competitions download -w -c data-science-bowl-2018
 unzip stage1_test.zip -d ./test
 unzip stage1_train.zip -d ./train
 unzip stage1_train_labels.csv.zip -d ./train
+
+git clone https://github.com/lopuhin/kaggle-dsbowl-2018-dataset-fixes fixed_train
