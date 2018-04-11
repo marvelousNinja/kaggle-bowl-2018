2b81d0729adab958dfd413133ba1a04b8fcbf514
Alexander Sologub <alexsologoub@gmail.com>
2018-04-11 13:24:11 +0300

    Install Cython before everything else

--- colab-setup.sh
+++ colab-setup.sh
@@ -4,6 +4,7 @@ export DEBCONF_NONINTERACTIVE_SEEN=true
 
 apt-get install -y python3.6-tk nano htop screen nvidia-cuda-toolkit --no-install-recommends
 pip3 install http://download.pytorch.org/whl/cu80/torch-0.3.1-cp36-cp36m-linux_x86_64.whl
+pip3 install Cython
 pip3 install -r requirements.txt
 
 ./make.sh
